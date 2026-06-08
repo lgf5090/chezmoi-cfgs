fe9fc26024c294c2cad33c74f4656676c88ecf2a
lgf5090 <lgf5090@gmail.com>
2026-06-08 19:56:15 +0800

    Use writable zsh completion dump cache

--- dot_config/zsh/conf.d/30-completion.zsh
+++ dot_config/zsh/conf.d/30-completion.zsh
@@ -1,12 +1,18 @@
 autoload -Uz compinit
 
-_zcompdump="$XDG_CACHE_HOME/zsh/zcompdump-${ZSH_VERSION}"
+_zcompdump_dir="${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
+if ! { [[ -d $_zcompdump_dir ]] || mkdir -p "$_zcompdump_dir" 2>/dev/null; } || [[ ! -w $_zcompdump_dir ]]; then
+  _zcompdump_dir="${TMPDIR:-/tmp}/zsh-${UID:-$USER}"
+  mkdir -p "$_zcompdump_dir" 2>/dev/null
+fi
+
+_zcompdump="$_zcompdump_dir/zcompdump-${ZSH_VERSION}"
 if [[ ! -s $_zcompdump || $_zcompdump -ot "$ZSH_CONFIG_DIR/config.zsh" ]]; then
   compinit -d "$_zcompdump"
 else
   compinit -C -d "$_zcompdump"
 fi
-unset _zcompdump
+unset _zcompdump _zcompdump_dir
 
 LISTMAX=999999
 zstyle ':completion:*' menu select
