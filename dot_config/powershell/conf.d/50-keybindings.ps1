2953c4c14621ff5bf1722c3eca5017856efa0800
lgf5090 <lgf5090@gmail.com>
2026-06-08 19:41:03 +0800

    Avoid PowerShell keybinding command lookups

--- dot_config/powershell/conf.d/50-keybindings.ps1
+++ dot_config/powershell/conf.d/50-keybindings.ps1
@@ -1,8 +1,5 @@
-if (Get-Command Set-PSReadLineOption -ErrorAction SilentlyContinue) {
+if (Get-Module -Name PSReadLine) {
     Set-PSReadLineOption -EditMode Vi -ErrorAction SilentlyContinue
-}
-
-if (Get-Command Set-PSReadLineKeyHandler -ErrorAction SilentlyContinue) {
     Set-PSReadLineKeyHandler -Chord 'Ctrl+a' -Function BeginningOfLine -ErrorAction SilentlyContinue
     Set-PSReadLineKeyHandler -Chord 'Ctrl+e' -Function EndOfLine -ErrorAction SilentlyContinue
     Set-PSReadLineKeyHandler -Chord 'Ctrl+LeftArrow' -Function BackwardWord -ErrorAction SilentlyContinue
