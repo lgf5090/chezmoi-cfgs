065d7bcc3c1e06b7d170b4989142026aa476b517
lgf5090 <lgf5090@gmail.com>
2026-06-08 15:14:48 +0800

    Expand PowerShell toolchain path setup

---
+++ dot_config/powershell/conf.d/12-toolchains.ps1
@@ -0,0 +1,168 @@
+# Language and toolchain environment variables used by 15-path.ps1.
+# Existing values are respected so ~/.envs or parent shells can override.
+
+$homeDir = [Environment]::GetFolderPath('UserProfile')
+$xdgDataHome = if ([string]::IsNullOrWhiteSpace($env:XDG_DATA_HOME)) {
+    Join-Path $homeDir '.local/share'
+} else {
+    $env:XDG_DATA_HOME
+}
+
+function Set-EnvIfMissing {
+    param(
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter(Mandatory)][string]$Value
+    )
+
+    if (-not [string]::IsNullOrWhiteSpace([Environment]::GetEnvironmentVariable($Name, 'Process'))) {
+        return
+    }
+    if ([string]::IsNullOrWhiteSpace($Value)) {
+        return
+    }
+
+    [Environment]::SetEnvironmentVariable($Name, $Value, 'Process')
+}
+
+function Set-EnvDirIfMissing {
+    param(
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter(ValueFromRemainingArguments)][string[]]$Path
+    )
+
+    if (-not [string]::IsNullOrWhiteSpace([Environment]::GetEnvironmentVariable($Name, 'Process'))) {
+        return
+    }
+
+    foreach ($item in $Path) {
+        if ([string]::IsNullOrWhiteSpace($item)) { continue }
+        if (-not (Test-Path -LiteralPath $item -PathType Container)) { continue }
+        [Environment]::SetEnvironmentVariable($Name, (Resolve-Path -LiteralPath $item).ProviderPath, 'Process')
+        return
+    }
+}
+
+function Add-EnvPathPrepend {
+    param(
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter(Mandatory)][string]$Path
+    )
+
+    if (-not (Test-Path -LiteralPath $Path -PathType Container)) {
+        return
+    }
+
+    $resolved = (Resolve-Path -LiteralPath $Path).ProviderPath
+    $current = [Environment]::GetEnvironmentVariable($Name, 'Process')
+    $separator = [IO.Path]::PathSeparator
+    $comparer = Get-PathComparer
+    $entries = [System.Collections.Generic.List[string]]::new()
+    $entries.Add($resolved)
+
+    if (-not [string]::IsNullOrWhiteSpace($current)) {
+        foreach ($entry in ($current -split [Regex]::Escape($separator))) {
+            if ([string]::IsNullOrWhiteSpace($entry)) { continue }
+            if ($comparer.Equals($entry, $resolved)) { continue }
+            $entries.Add($entry)
+        }
+    }
+
+    [Environment]::SetEnvironmentVariable($Name, ($entries -join $separator), 'Process')
+}
+
+Set-EnvIfMissing -Name 'NPM_CONFIG_PREFIX' -Value (Join-Path $homeDir '.npm-global')
+Set-EnvIfMissing -Name 'PNPM_HOME' -Value (Join-Path $homeDir '.pnpm-global')
+
+Set-EnvDirIfMissing -Name 'FNM_DIR' `
+    (Join-Path $xdgDataHome 'fnm') `
+    (Join-Path $homeDir '.fnm')
+
+Set-EnvDirIfMissing -Name 'VOLTA_HOME' (Join-Path $homeDir '.volta')
+Set-EnvDirIfMissing -Name 'BUN_INSTALL' (Join-Path $homeDir '.bun')
+Set-EnvDirIfMissing -Name 'DENO_INSTALL' (Join-Path $homeDir '.deno')
+
+Set-EnvIfMissing -Name 'GOPATH' -Value (Join-Path $homeDir 'go')
+Set-EnvDirIfMissing -Name 'GOROOT' `
+    '/home/linuxbrew/.linuxbrew/opt/go/libexec' `
+    '/opt/homebrew/opt/go/libexec' `
+    '/usr/local/go' `
+    (Join-Path $homeDir '.local/go')
+
+Set-EnvDirIfMissing -Name 'ANACONDA_HOME' `
+    (Join-Path $homeDir 'anaconda3') `
+    (Join-Path $homeDir 'miniconda3') `
+    '/opt/anaconda3' `
+    '/opt/miniconda3'
+
+Set-EnvDirIfMissing -Name 'POETRY_HOME' (Join-Path $homeDir '.poetry')
+Set-EnvDirIfMissing -Name 'PYENV_ROOT' (Join-Path $homeDir '.pyenv')
+
+$asdfCandidates = @(
+    (Join-Path $homeDir '.asdf')
+)
+if (-not [string]::IsNullOrWhiteSpace($env:HOMEBREW_PREFIX)) {
+    $asdfCandidates += (Join-Path $env:HOMEBREW_PREFIX 'opt/asdf/libexec')
+}
+$asdfCandidates += @(
+    '/home/linuxbrew/.linuxbrew/opt/asdf/libexec',
+    '/opt/homebrew/opt/asdf/libexec',
+    '/usr/local/opt/asdf/libexec'
+)
+Set-EnvDirIfMissing -Name 'ASDF_DIR' $asdfCandidates
+
+if ([string]::IsNullOrWhiteSpace($env:ASDF_DATA_DIR) -and -not [string]::IsNullOrWhiteSpace($env:ASDF_DIR)) {
+    if ((Get-PathComparer).Equals($env:ASDF_DIR, (Join-Path $homeDir '.asdf'))) {
+        $env:ASDF_DATA_DIR = $env:ASDF_DIR
+    } else {
+        $env:ASDF_DATA_DIR = Join-Path $xdgDataHome 'asdf'
+    }
+}
+
+Set-EnvDirIfMissing -Name 'RBENV_ROOT' (Join-Path $homeDir '.rbenv')
+Set-EnvDirIfMissing -Name 'NODENV_ROOT' (Join-Path $homeDir '.nodenv')
+Set-EnvDirIfMissing -Name 'GOENV_ROOT' (Join-Path $homeDir '.goenv')
+Set-EnvDirIfMissing -Name 'JENV_ROOT' (Join-Path $homeDir '.jenv')
+Set-EnvDirIfMissing -Name 'SDKMAN_DIR' (Join-Path $homeDir '.sdkman')
+
+if ([string]::IsNullOrWhiteSpace($env:JAVA_HOME)) {
+    if (Test-Path -LiteralPath '/usr/libexec/java_home' -PathType Leaf) {
+        $javaHome = & /usr/libexec/java_home 2>$null
+        if (-not [string]::IsNullOrWhiteSpace($javaHome)) {
+            $env:JAVA_HOME = $javaHome
+        }
+    } else {
+        Set-EnvDirIfMissing -Name 'JAVA_HOME' `
+            '/usr/lib/jvm/default-java' `
+            '/usr/lib/jvm/default' `
+            '/usr/lib/jvm/java-21-openjdk-amd64' `
+            '/usr/lib/jvm/java-17-openjdk-amd64' `
+            '/usr/lib/jvm/java-11-openjdk-amd64'
+    }
+}
+
+switch ($global:ShellsOS) {
+    { $_ -in @('linux', 'wsl') } {
+        foreach ($libDir in @('/usr/lib/x86_64-linux-gnu', '/usr/lib/aarch64-linux-gnu')) {
+            if (-not (Test-Path -LiteralPath $libDir -PathType Container)) { continue }
+
+            Add-EnvPathPrepend -Name 'LIBRARY_PATH' -Path $libDir
+            Add-EnvPathPrepend -Name 'LD_LIBRARY_PATH' -Path $libDir
+
+            if (" $env:RUSTFLAGS " -notlike "* -L $libDir *") {
+                $env:RUSTFLAGS = if ([string]::IsNullOrWhiteSpace($env:RUSTFLAGS)) {
+                    "-L $libDir"
+                } else {
+                    "-L $libDir $env:RUSTFLAGS"
+                }
+            }
+            break
+        }
+    }
+}
+
+Set-EnvIfMissing -Name 'DOCKER_BUILDKIT' -Value '1'
+Set-EnvIfMissing -Name 'COMPOSE_DOCKER_CLI_BUILD' -Value '1'
+
+Remove-Item Function:\Set-EnvIfMissing -ErrorAction SilentlyContinue
+Remove-Item Function:\Set-EnvDirIfMissing -ErrorAction SilentlyContinue
+Remove-Item Function:\Add-EnvPathPrepend -ErrorAction SilentlyContinue
--- dot_config/powershell/conf.d/15-path.ps1
+++ dot_config/powershell/conf.d/15-path.ps1
@@ -1,25 +1,101 @@
 $homeDir = [Environment]::GetFolderPath('UserProfile')
 
 Add-PathAppend `
+    (Join-Path $homeDir '.lmstudio/bin') `
     (Join-Path $homeDir '.local/bin') `
     (Join-Path $homeDir 'bin') `
-    (Join-Path $homeDir 'Applications')
+    (Join-Path $homeDir 'Applications') `
+    (Join-Path $homeDir '.local/Applications')
 
 Add-PathPrepend `
-    (Join-Path $homeDir '.cargo/bin') `
+    $(if ($env:ASDF_DIR) { Join-Path $env:ASDF_DIR 'bin' }) `
+    $(if ($env:RBENV_ROOT) { Join-Path $env:RBENV_ROOT 'bin' }) `
+    $(if ($env:NODENV_ROOT) { Join-Path $env:NODENV_ROOT 'bin' }) `
+    $(if ($env:GOENV_ROOT) { Join-Path $env:GOENV_ROOT 'bin' }) `
+    $(if ($env:JENV_ROOT) { Join-Path $env:JENV_ROOT 'bin' }) `
+    (Join-Path $(if ($env:CARGO_HOME) { $env:CARGO_HOME } else { Join-Path $homeDir '.cargo' }) 'bin') `
     (Join-Path $homeDir '.rd/bin') `
     (Join-Path $homeDir '.opencode/bin')
 
-if ($script:IsWindowsPlatform) {
+$cargoEnv = Join-Path $homeDir '.cargo/env.ps1'
+if (Test-Path -LiteralPath $cargoEnv -PathType Leaf) {
+    . $cargoEnv
+}
+
+Add-PathPrepend `
+    $(if ($env:BUN_INSTALL) { Join-Path $env:BUN_INSTALL 'bin' }) `
+    $(if ($env:DENO_INSTALL) { Join-Path $env:DENO_INSTALL 'bin' }) `
+    $(if ($env:NPM_CONFIG_PREFIX) { Join-Path $env:NPM_CONFIG_PREFIX 'bin' }) `
+    $env:PNPM_HOME `
+    (Join-Path $homeDir '.yarn/bin') `
+    (Join-Path $homeDir '.config/yarn/global/node_modules/.bin') `
+    $(if ($env:VOLTA_HOME) { Join-Path $env:VOLTA_HOME 'bin' }) `
+    (Join-Path $homeDir '.volta/bin') `
+    $env:FNM_DIR `
+    (Join-Path $homeDir '.local/share/npm/bin')
+
+Add-PathPrepend `
+    $(if ($env:PYENV_ROOT) { Join-Path $env:PYENV_ROOT 'bin' }) `
+    $(if ($env:ANACONDA_HOME) { Join-Path $env:ANACONDA_HOME 'bin' }) `
+    $(if ($env:POETRY_HOME) { Join-Path $env:POETRY_HOME 'bin' }) `
+    (Join-Path $homeDir '.poetry/bin') `
+    (Join-Path $homeDir '.local/pipx/bin')
+
+Add-PathPrepend `
+    $(if ($env:GOPATH) { Join-Path $env:GOPATH 'bin' }) `
+    $(if ($env:GOROOT) { Join-Path $env:GOROOT 'bin' })
+
+switch ($global:ShellsOS) {
+    { $_ -in @('linux', 'wsl') } {
+        Add-PathAppend `
+            '/snap/bin' `
+            '/var/lib/snapd/snap/bin' `
+            '/var/lib/flatpak/exports/bin' `
+            (Join-Path $homeDir '.local/share/flatpak/exports/bin') `
+            '/opt/bin'
+    }
+}
+
+switch ($global:ShellsOS) {
+    'wsl' {
+        Add-PathAppend `
+            '/mnt/c/Program Files/Microsoft VS Code/bin' `
+            "/mnt/c/Users/$env:USER/AppData/Local/Programs/Microsoft VS Code/bin"
+    }
+    'windows' {
+        Add-PathPrepend `
+            (Join-Path $homeDir 'scoop/shims') `
+            $(if ($env:PROGRAMDATA) { Join-Path $env:PROGRAMDATA 'scoop/shims' }) `
+            $(if ($env:PROGRAMDATA) { Join-Path $env:PROGRAMDATA 'chocolatey/bin' }) `
+            $(if ($env:LOCALAPPDATA) { Join-Path $env:LOCALAPPDATA 'Microsoft/WindowsApps' }) `
+            $(if ($env:APPDATA) { Join-Path $env:APPDATA 'npm' })
+    }
+}
+
+Add-PathPrepend `
+    (Join-Path $homeDir '.nix-profile/bin') `
+    '/run/current-system/sw/bin' `
+    '/nix/var/nix/profiles/default/bin'
+
+foreach ($brew in @(
+    '/home/linuxbrew/.linuxbrew/bin/brew',
+    (Join-Path $homeDir '.linuxbrew/bin/brew'),
+    '/opt/homebrew/bin/brew',
+    '/usr/local/bin/brew'
+)) {
+    if (-not (Test-Path -LiteralPath $brew -PathType Leaf)) { continue }
+
+    $brewBin = Split-Path -Parent $brew
+    $brewPrefix = Split-Path -Parent $brewBin
     Add-PathPrepend `
-        (Join-Path $homeDir 'scoop/shims') `
-        (Join-Path $env:LOCALAPPDATA 'Microsoft/WindowsApps') `
-        (Join-Path $env:APPDATA 'npm')
-} else {
-    Add-PathPrepend `
-        '/home/linuxbrew/.linuxbrew/bin' `
-        '/home/linuxbrew/.linuxbrew/sbin' `
-        '/opt/homebrew/bin' `
-        '/opt/homebrew/sbin' `
-        '/usr/local/bin'
+        (Join-Path $brewPrefix 'bin') `
+        (Join-Path $brewPrefix 'sbin')
+    $env:HOMEBREW_PREFIX = $brewPrefix
+    $env:HOMEBREW_CELLAR = Join-Path $brewPrefix 'Cellar'
+    $env:HOMEBREW_REPOSITORY = if ((Split-Path -Leaf $brewPrefix) -eq 'Homebrew') {
+        $brewPrefix
+    } else {
+        Join-Path $brewPrefix 'Homebrew'
+    }
+    break
 }
