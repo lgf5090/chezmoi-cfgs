4d6bf4e544fc1096ce43b99a36acac4d565accb8
lgf5090 <lgf5090@gmail.com>
2026-06-11 01:41:30 +0800

    powershell: load PSReadLine only for console

--- dot_config/powershell/conf.d/20-history.ps1
+++ dot_config/powershell/conf.d/20-history.ps1
@@ -4,7 +4,18 @@ if (-not [System.IO.Directory]::Exists($historyDir)) {
     [System.IO.Directory]::CreateDirectory($historyDir) | Out-Null
 }
 
-Import-Module PSReadLine -ErrorAction SilentlyContinue
-if (Get-Module -Name PSReadLine) {
+$loadPSReadLine = (
+    $env:POWERSHELL_LOAD_PSREADLINE -eq '1' -or (
+        $env:POWERSHELL_LOAD_PSREADLINE -ne '0' -and
+        -not [Console]::IsInputRedirected -and
+        -not [Console]::IsOutputRedirected
+    )
+)
+
+if ($loadPSReadLine) {
+    Import-Module PSReadLine -ErrorAction SilentlyContinue
+}
+
+if ($loadPSReadLine -and (Get-Module -Name PSReadLine)) {
     Set-PSReadLineOption -HistorySavePath $historyFile -HistorySaveStyle SaveIncrementally -ErrorAction SilentlyContinue
 }
