937bda07da9cdaa0da6767c373a916c4d9572fc1
lgf5090 <lgf5090@gmail.com>
2026-06-10 21:11:27 +0800

    fish: make fnm hook opt-in

--- dot_config/fish/conf.d/73-fnm.fish
+++ dot_config/fish/conf.d/73-fnm.fish
@@ -1,5 +1,17 @@
 set -q FNM_DIR; and _fpath_prepend "$FNM_DIR"
 
-if command -q fnm
-    fnm env --use-on-cd --shell fish 2>/dev/null | source
+set -q FISH_FNM_ACTIVATE; or set -gx FISH_FNM_ACTIVATE default
+
+switch (string lower -- "$FISH_FNM_ACTIVATE")
+    case full use-on-cd 1 yes true
+        if command -q fnm
+            fnm env --use-on-cd --shell fish 2>/dev/null | source
+        end
+    case env
+        if command -q fnm
+            fnm env --shell fish 2>/dev/null | source
+        end
+    case none 0 no false
+    case '*'
+        set -q FNM_DIR; and _fpath_prepend "$FNM_DIR/aliases/default/bin"
 end
