96ac94b6b1a8748185e83aa781ba2105f8a45e22
lgf5090 <lgf5090@gmail.com>
2026-06-10 13:21:16 +0800

    zsh: cache direnv hook script

--- dot_config/zsh/conf.d/78-direnv.zsh
+++ dot_config/zsh/conf.d/78-direnv.zsh
@@ -1,5 +1,27 @@
 if (( $+commands[direnv] )) && [[ -o interactive ]]; then
-  __zsh_direnv_hook=$(direnv hook zsh 2>/dev/null || :)
-  [[ -n $__zsh_direnv_hook ]] && eval "$__zsh_direnv_hook"
-  unset __zsh_direnv_hook
+  __zsh_direnv=${commands[direnv]}
+  __zsh_direnv_cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
+  __zsh_direnv_cache_key=${__zsh_direnv:A}
+  __zsh_direnv_cache_key=${__zsh_direnv_cache_key//\//%}
+  __zsh_direnv_cache="$__zsh_direnv_cache_dir/direnv-hook-${__zsh_direnv_cache_key}.zsh"
+
+  if [[ -s $__zsh_direnv_cache && $__zsh_direnv_cache -nt $__zsh_direnv ]]; then
+    source "$__zsh_direnv_cache"
+  else
+    __zsh_direnv_hook=$("$__zsh_direnv" hook zsh 2>/dev/null || :)
+    if [[ -n $__zsh_direnv_hook ]]; then
+      eval "$__zsh_direnv_hook"
+      if [[ -d $__zsh_direnv_cache_dir || -w ${__zsh_direnv_cache_dir:h} ]]; then
+        [[ -d $__zsh_direnv_cache_dir ]] || mkdir -p "$__zsh_direnv_cache_dir" 2>/dev/null
+        if [[ -d $__zsh_direnv_cache_dir && -w $__zsh_direnv_cache_dir ]]; then
+          __zsh_direnv_cache_tmp="$__zsh_direnv_cache.tmp.$$"
+          print -r -- "$__zsh_direnv_hook" > "$__zsh_direnv_cache_tmp" \
+            && command mv -f "$__zsh_direnv_cache_tmp" "$__zsh_direnv_cache" 2>/dev/null \
+            || command rm -f "$__zsh_direnv_cache_tmp"
+        fi
+      fi
+    fi
+  fi
+  unset __zsh_direnv __zsh_direnv_hook __zsh_direnv_cache_dir \
+    __zsh_direnv_cache_key __zsh_direnv_cache __zsh_direnv_cache_tmp
 fi
