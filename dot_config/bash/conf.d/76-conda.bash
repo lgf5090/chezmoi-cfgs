d64b0c06c62f038a370d3ca605e250fabedc0735
lgf5090 <lgf5090@gmail.com>
2026-06-10 19:50:02 +0800

    bash: avoid conda discovery by default

--- dot_config/bash/conf.d/76-conda.bash
+++ dot_config/bash/conf.d/76-conda.bash
@@ -1,7 +1,7 @@
-if [[ -n ${ANACONDA_HOME:-} && -r $ANACONDA_HOME/etc/profile.d/conda.sh ]]; then
+if [[ -n ${ANACONDA_HOME:-} && -x $ANACONDA_HOME/bin/conda ]]; then
   __BASH_CONDA_EXE=$ANACONDA_HOME/bin/conda
-elif command -v conda >/dev/null 2>&1; then
-  __BASH_CONDA_EXE=$(command -v conda)
+elif [[ ${BASH_CONDA_DISCOVERY:-0} == 1 ]]; then
+  __BASH_CONDA_EXE=$(command -v conda 2>/dev/null || :)
 fi
 
 if [[ -n ${__BASH_CONDA_EXE:-} && -x $__BASH_CONDA_EXE ]]; then
@@ -24,17 +24,29 @@ if [[ -n ${__BASH_CONDA_EXE:-} && -x $__BASH_CONDA_EXE ]]; then
     conda "$@"
   }
 
-  if command -v mamba >/dev/null 2>&1; then
+  __BASH_MAMBA_EXE=
+  if [[ -n ${ANACONDA_HOME:-} && -x $ANACONDA_HOME/bin/mamba ]]; then
+    __BASH_MAMBA_EXE=$ANACONDA_HOME/bin/mamba
+  elif [[ ${BASH_CONDA_DISCOVERY:-0} == 1 ]]; then
+    __BASH_MAMBA_EXE=$(command -v mamba 2>/dev/null || :)
+  fi
+
+  if [[ -n $__BASH_MAMBA_EXE ]]; then
     mamba() {
       _bconda_load
       mamba "$@"
     }
   fi
+  unset __BASH_MAMBA_EXE
 fi
 
-if command -v micromamba >/dev/null 2>&1; then
-  __BASH_MICROMAMBA_EXE=$(command -v micromamba)
+if [[ -n ${MICROMAMBA_EXE:-} && -x $MICROMAMBA_EXE ]]; then
+  __BASH_MICROMAMBA_EXE=$MICROMAMBA_EXE
+elif [[ ${BASH_CONDA_DISCOVERY:-0} == 1 ]]; then
+  __BASH_MICROMAMBA_EXE=$(command -v micromamba 2>/dev/null || :)
+fi
 
+if [[ -n ${__BASH_MICROMAMBA_EXE:-} && -x $__BASH_MICROMAMBA_EXE ]]; then
   micromamba() {
     local __bash_micromamba_setup
     unset -f micromamba 2>/dev/null
