0fe6d9d1760dedbba70ee58e4e6dd1c6819764c7
lgf5090 <lgf5090@gmail.com>
2026-06-10 13:30:14 +0800

    zsh: avoid pyenv path lookup by default

--- dot_config/zsh/conf.d/72-pyenv.zsh
+++ dot_config/zsh/conf.d/72-pyenv.zsh
@@ -2,12 +2,20 @@ if [[ -n ${PYENV_ROOT:-} ]]; then
   _zpath_prepend "$PYENV_ROOT/bin"
 fi
 
-if (( $+commands[pyenv] )); then
-  __zsh_pyenv_init=$(pyenv init - zsh 2>/dev/null || pyenv init - 2>/dev/null || :)
+__zsh_pyenv=
+if [[ -n ${PYENV_ROOT:-} && -x $PYENV_ROOT/bin/pyenv ]]; then
+  __zsh_pyenv=$PYENV_ROOT/bin/pyenv
+elif [[ ${ZSH_PYENV_DISCOVERY:-0} == 1 ]] && (( $+commands[pyenv] )); then
+  __zsh_pyenv=${commands[pyenv]}
+fi
+
+if [[ -n $__zsh_pyenv ]]; then
+  __zsh_pyenv_init=$("$__zsh_pyenv" init - zsh 2>/dev/null || "$__zsh_pyenv" init - 2>/dev/null || :)
   [[ -n $__zsh_pyenv_init ]] && eval "$__zsh_pyenv_init"
   unset __zsh_pyenv_init
 
-  __zsh_pyenv_virtualenv_init=$(pyenv virtualenv-init - zsh 2>/dev/null || pyenv virtualenv-init - 2>/dev/null || :)
+  __zsh_pyenv_virtualenv_init=$("$__zsh_pyenv" virtualenv-init - zsh 2>/dev/null || "$__zsh_pyenv" virtualenv-init - 2>/dev/null || :)
   [[ -n $__zsh_pyenv_virtualenv_init ]] && eval "$__zsh_pyenv_virtualenv_init"
   unset __zsh_pyenv_virtualenv_init
 fi
+unset __zsh_pyenv
