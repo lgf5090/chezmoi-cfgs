767b6e7b28289717f68a863ac823e43d19767434
lgf5090 <lgf5090@gmail.com>
2026-06-10 13:13:28 +0800

    zsh: make fnm hook opt-in

--- dot_config/zsh/conf.d/73-fnm.zsh
+++ dot_config/zsh/conf.d/73-fnm.zsh
@@ -2,8 +2,25 @@ if [[ -n ${FNM_DIR:-} ]]; then
   _zpath_prepend "$FNM_DIR"
 fi
 
-if (( $+commands[fnm] )); then
-  __zsh_fnm_env=$(fnm env --use-on-cd --shell zsh 2>/dev/null || :)
-  [[ -n $__zsh_fnm_env ]] && eval "$__zsh_fnm_env"
-  unset __zsh_fnm_env
-fi
+: "${ZSH_FNM_ACTIVATE:=default}"
+
+case ${ZSH_FNM_ACTIVATE:l} in
+  full|use-on-cd|1|yes|true)
+    if (( $+commands[fnm] )); then
+      __zsh_fnm_env=$(fnm env --use-on-cd --shell zsh 2>/dev/null || :)
+      [[ -n $__zsh_fnm_env ]] && eval "$__zsh_fnm_env"
+      unset __zsh_fnm_env
+    fi
+    ;;
+  env)
+    if (( $+commands[fnm] )); then
+      __zsh_fnm_env=$(fnm env --shell zsh 2>/dev/null || :)
+      [[ -n $__zsh_fnm_env ]] && eval "$__zsh_fnm_env"
+      unset __zsh_fnm_env
+    fi
+    ;;
+  none|0|no|false) ;;
+  *)
+    [[ -n ${FNM_DIR:-} ]] && _zpath_prepend "$FNM_DIR/aliases/default/bin"
+    ;;
+esac
