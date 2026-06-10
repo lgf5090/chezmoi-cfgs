06cf00b7cd18c1f6840599d9f5fd9783928a2792
lgf5090 <lgf5090@gmail.com>
2026-06-10 19:10:27 +0800

    bash: make fnm hook opt-in

--- dot_config/bash/conf.d/73-fnm.bash
+++ dot_config/bash/conf.d/73-fnm.bash
@@ -2,8 +2,25 @@ if [[ -n ${FNM_DIR:-} ]]; then
   _bpath_prepend "$FNM_DIR"
 fi
 
-if command -v fnm >/dev/null 2>&1; then
-  __bash_fnm_env=$(fnm env --use-on-cd --shell bash 2>/dev/null || :)
-  [[ -n $__bash_fnm_env ]] && eval "$__bash_fnm_env"
-  unset __bash_fnm_env
-fi
+: "${BASH_FNM_ACTIVATE:=default}"
+
+case ${BASH_FNM_ACTIVATE,,} in
+  full|use-on-cd|1|yes|true)
+    if command -v fnm >/dev/null 2>&1; then
+      __bash_fnm_env=$(fnm env --use-on-cd --shell bash 2>/dev/null || :)
+      [[ -n $__bash_fnm_env ]] && eval "$__bash_fnm_env"
+      unset __bash_fnm_env
+    fi
+    ;;
+  env)
+    if command -v fnm >/dev/null 2>&1; then
+      __bash_fnm_env=$(fnm env --shell bash 2>/dev/null || :)
+      [[ -n $__bash_fnm_env ]] && eval "$__bash_fnm_env"
+      unset __bash_fnm_env
+    fi
+    ;;
+  none|0|no|false) ;;
+  *)
+    [[ -n ${FNM_DIR:-} ]] && _bpath_prepend "$FNM_DIR/aliases/default/bin"
+    ;;
+esac
