405cb1fff4eb330aa90908e7850002919019773e
lgf5090 <lgf5090@gmail.com>
2026-06-10 19:47:59 +0800

    bash: avoid env manager discovery by default

--- dot_config/bash/conf.d/75-env-managers.bash
+++ dot_config/bash/conf.d/75-env-managers.bash
@@ -1,14 +1,34 @@
-for __bash_env_manager in rbenv nodenv goenv; do
-  if command -v "$__bash_env_manager" >/dev/null 2>&1; then
-    __bash_env_manager_init=$("$__bash_env_manager" init - bash 2>/dev/null || "$__bash_env_manager" init - 2>/dev/null || :)
+for __bash_env_manager_spec in rbenv:RBENV_ROOT nodenv:NODENV_ROOT goenv:GOENV_ROOT; do
+  __bash_env_manager=${__bash_env_manager_spec%%:*}
+  __bash_env_root_var=${__bash_env_manager_spec#*:}
+  __bash_env_root=${!__bash_env_root_var:-}
+  __bash_env_manager_exe=
+
+  if [[ -n $__bash_env_root && -x $__bash_env_root/bin/$__bash_env_manager ]]; then
+    __bash_env_manager_exe=$__bash_env_root/bin/$__bash_env_manager
+  elif [[ ${BASH_ENV_MANAGER_DISCOVERY:-0} == 1 ]]; then
+    __bash_env_manager_exe=$(command -v "$__bash_env_manager" 2>/dev/null || :)
+  fi
+
+  if [[ -n $__bash_env_manager_exe ]]; then
+    __bash_env_manager_init=$("$__bash_env_manager_exe" init - bash 2>/dev/null || "$__bash_env_manager_exe" init - 2>/dev/null || :)
     [[ -n $__bash_env_manager_init ]] && eval "$__bash_env_manager_init"
     unset __bash_env_manager_init
   fi
 done
-unset __bash_env_manager
+unset __bash_env_manager_spec __bash_env_manager __bash_env_root_var \
+  __bash_env_root __bash_env_manager_exe
 
-if command -v jenv >/dev/null 2>&1; then
-  __bash_jenv_init=$(jenv init - 2>/dev/null || :)
+__bash_jenv=
+if [[ -n ${JENV_ROOT:-} && -x $JENV_ROOT/bin/jenv ]]; then
+  __bash_jenv=$JENV_ROOT/bin/jenv
+elif [[ ${BASH_ENV_MANAGER_DISCOVERY:-0} == 1 ]]; then
+  __bash_jenv=$(command -v jenv 2>/dev/null || :)
+fi
+
+if [[ -n $__bash_jenv ]]; then
+  __bash_jenv_init=$("$__bash_jenv" init - 2>/dev/null || :)
   [[ -n $__bash_jenv_init ]] && eval "$__bash_jenv_init"
   unset __bash_jenv_init
 fi
+unset __bash_jenv
