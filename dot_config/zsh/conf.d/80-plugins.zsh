c4970384ebbae9f8937d57b7f7fb83c3bcb6a14a
lgf5090 <lgf5090@gmail.com>
2026-06-10 12:43:48 +0800

    zsh: defer interactive plugin loading

--- dot_config/zsh/conf.d/80-plugins.zsh
+++ dot_config/zsh/conf.d/80-plugins.zsh
@@ -1,4 +1,30 @@
-_zplugin zsh-users zsh-autosuggestions
-_zplugin zsh-users zsh-history-substring-search
-_zplugin jeffreytse zsh-vi-mode
-_zplugin zdharma-continuum fast-syntax-highlighting
+_zplugins_load() {
+  (( ${+__zsh_plugins_loaded} )) && return 0
+  typeset -g __zsh_plugins_loaded=1
+
+  _zplugin zsh-users zsh-autosuggestions
+  _zplugin zsh-users zsh-history-substring-search
+  _zplugin jeffreytse zsh-vi-mode
+  _zplugin zdharma-continuum fast-syntax-highlighting
+}
+
+case ${ZSH_PLUGIN_LOAD:-defer} in
+  none|0|no|false) ;;
+  sync|eager|1|yes|true)
+    _zplugins_load
+    ;;
+  *)
+    if [[ -o interactive ]]; then
+      autoload -Uz add-zle-hook-widget
+      _zplugins_load_deferred() {
+        add-zle-hook-widget -d zle-line-init _zplugins_load_deferred 2>/dev/null
+        _zplugins_load
+        zle reset-prompt 2>/dev/null || true
+      }
+      add-zle-hook-widget zle-line-init _zplugins_load_deferred 2>/dev/null \
+        || _zplugins_load
+    else
+      _zplugins_load
+    fi
+    ;;
+esac
